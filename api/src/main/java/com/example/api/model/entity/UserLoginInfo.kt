2dc62fb61803b9af7021d88183da4ddc28253c94
flipxonsoft <manishjobs1999@gmail.com>
2021-03-17 13:22:06 +0530

    [Feat]Add Login Feature in app

--- api/src/main/java/com/example/api/model/entity/UserLoginInfo.kt
+++ api/src/main/java/com/example/api/model/entity/UserLoginInfo.kt
@@ -1,11 +1,11 @@
-package com.example.api.model.entity
+package com.example.api.model.requests
 
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class UserLoginRequest(
+data class User(
     @Json(name = "email")
     val email: String,
     @Json(name = "password")
--- api/src/main/java/com/example/api/model/requests/LoginRequest.kt
+++ api/src/main/java/com/example/api/model/requests/LoginRequest.kt
@@ -1,12 +1,11 @@
 package com.example.api.model.requests
 
 
-import com.example.api.model.entity.UserLoginRequest
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class LoginRequest(
     @Json(name = "user")
-    val userLoginRequest: UserLoginRequest
+    val user: User
 )

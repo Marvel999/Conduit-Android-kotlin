ca648e69c0b9edef22f3d77ba28d05e2d23b477d
flipxonsoft <manishjobs1999@gmail.com>
2021-03-19 23:15:07 +0530

    [Feat] Add profile backend integration feature

--- app/src/main/java/com/example/blogoapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/blogoapp/ui/profile/ProfileFragment.kt
@@ -4,18 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.example.api.ConduitClient
 import com.example.blogoapp.R
+import com.example.blogoapp.data.UserSharedpreferences
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ProfileFragment : Fragment() {
 
     private lateinit var profileViewModel: ProfileViewModel
      private lateinit var fab:FloatingActionButton
+     private lateinit var userName:TextView
+     private lateinit var editBtn:ImageView
+     private lateinit var userBio:TextView
+     private lateinit var profileUserName:TextView
+     private lateinit var tvbio:TextView
+     private lateinit var emailId:TextView
+     private lateinit var profileImage:ImageView
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -31,12 +42,32 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         fab=view.findViewById(R.id.fab)
+        userName=view.findViewById(R.id.userName)
+        editBtn=view.findViewById(R.id.editBtn)
+        userBio=view.findViewById(R.id.userBio)
+        profileUserName=view.findViewById(R.id.profileUserName)
+        tvbio=view.findViewById(R.id.tvbio)
+        emailId=view.findViewById(R.id.emailId)
+        profileImage=view.findViewById(R.id.profileImage)
+        ConduitClient.authToken=UserSharedpreferences(requireContext()).token
 
     }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         fab.setOnClickListener {
             findNavController().navigate(R.id.updateUserInfo)
+        }
+        editBtn.setOnClickListener {
+            findNavController().navigate(R.id.updateUserInfo)
+        }
+        profileViewModel.getCurrentUserInfo()
+        profileViewModel.userRes.observe({lifecycle}){
+            userName.text=it.user.username
+            profileUserName.text=it.user.username
+            userBio.text=it.user.bio
+            tvbio.text=it.user.bio
+            emailId.text=it.user.email
+            Glide.with(this).load(it.user.image).circleCrop().into(profileImage)
         }
     }
 }
